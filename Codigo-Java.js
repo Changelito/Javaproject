ac1256f72668996e58c780d730f31e33bc681c2c
Luis Meza <luisangel0619@icloud.com>
2023-05-29 18:04:24 -0700

    pagina final

--- Codigo-Java.js
+++ Codigo-Java.js
@@ -23,3 +23,35 @@ function encriptar (){
 
 }
 
+
+function desencriptar (){
+  let texto = document.getElementById("texto-area").value;
+  let TituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+  let muñeco = document.getElementById("personaje");
+  let textoEncriptado = texto 
+  .replace(/enter/gi, "e" )
+  .replace(/imes/gi, "i")
+  .replace(/ai/gi, "a")
+  .replace(/ober/gi, "o" )
+  .replace(/ufat/gi, "u");
+
+  if (texto.length != 0){
+         let texto = document.getElementById("texto-area").value = textoEncriptado;
+         TituloMensaje.textContent = "Texto desencriptado con exito"; 
+         parrafo.textContent = "";
+         document.getElementById("personaje").src = "Imagenes/desencriptado.jpg";
+
+  } else {
+    document.getElementById("personaje").src = "Imagenes/Muñeco.png";
+           alert ("debes introducir un texto");
+           TituloMensaje.textContent = "Ningun texto fue encontrado " ;
+           parrafo.textContent = "Ingresa texto que desees encriptar o desencriptar";
+  }
+
+}
+function transferirTexto() {
+  var textoEntrada = document.getElementById("texto-area").value;
+  document.getElementById("texto-encriptado").value = textoEntrada;
+  document.getElementById("texto-area").value = "";
+}
